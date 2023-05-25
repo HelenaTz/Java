2a60d743ec6cba0cd7dc71fb82a4d61dd3a8a1aa
Helena Zimmermann <84276518+HelenaTz@users.noreply.github.com>
2023-05-25 13:08:10 -0300

    Delete If.java

--- If.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-
-public class If {
-    public static void main(String[] args) {
-        Scanner entrada = new scanner(System.in);
-        int i = entrada.nextInt();
-    }
-}
