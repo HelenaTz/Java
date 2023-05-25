085e9f419869ed6ce8dba08af34a7744590c7276
Helena Zimmermann <84276518+HelenaTz@users.noreply.github.com>
2023-05-25 13:07:10 -0300

    Update Console.java

--- Console.java
+++ Console.java
@@ -1,12 +1,1 @@
-package fundamentos;
-import java.util.Scanner;
 
-public class Console {
-	public static void main(String[] args) {
-		Scanner entrada = new Scanner(System.in);
-		System.out.println("Insira a idade");
-		int idade = entrada.nextInt();
-		
-		System.out.println("Idade" + idade);
-	}
-}
